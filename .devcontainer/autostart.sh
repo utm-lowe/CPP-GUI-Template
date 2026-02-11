056fdb2fe07027b37d91990ed7923e4ca4c64b3d
Robert Lowe <pngwen@gmail.com>
2026-02-11 18:16:16 +0000

    switching to fvwm

--- .devcontainer/autostart.sh
+++ .devcontainer/autostart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-tigervncserver -xstartup /usr/bin/fluxbox -SecurityTypes VncAuth,TLSVnc -geometry 1280x720 -localhost no :1 -SecurityTypes None --I-KNOW-THIS-IS-INSECURE
+tigervncserver -xstartup /usr/bin/fvwm -SecurityTypes VncAuth,TLSVnc -geometry 1280x720 -localhost no :1 -SecurityTypes None --I-KNOW-THIS-IS-INSECURE
 websockify -D --web=/usr/share/novnc/ --cert=/home/ubuntu/novnc.pem 6080 localhost:5901
--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt update
-sudo apt-get install -y doxygen fluxbox xterm tigervnc-standalone-server novnc python3-websockify
+sudo apt-get install -y doxygen fvwm x11-apps xbill xterm tigervnc-standalone-server novnc python3-websockify
 sudo ln -s /usr/share/novnc/vnc.html /usr/share/novnc/index.html
