5502771c27cec6b350e9346deb9af60d0df11c02
Robert Lowe <pngwen@gmail.com>
2026-02-11 17:57:23 +0000

    removing numpy

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt update
-sudo apt-get install -y doxygen fluxbox xterm tigervnc-standalone-server novnc python3-websockify python3-numpy
+sudo apt-get install -y doxygen fluxbox xterm tigervnc-standalone-server novnc python3-websockify
 sudo ln -s /usr/share/novnc/vnc.html /usr/share/novnc/index.html
